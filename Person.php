6b481095e81dc4ffa16cb654bdbeb3994d088246
Kushal Hada <kushaldeveloper@gmail.com>
2017-08-14 15:58:33 -0400

    add new class

---
+++ Person.php
@@ -0,0 +1,26 @@
+<?php
+
+class Person {
+
+  /**
+   * @var PDO
+   */
+  private $pdo;
+
+  public function __construct(PDO $pdo) {
+    $this->pdo = $pdo;
+  }
+
+  public function hello($what = 'World') {
+    $sql = "INSERT INTO person VALUES (" . $this->pdo->quote($what) . ")";
+    $this->pdo->query($sql);
+    return "Hello $what";
+  }
+
+
+  public function what() {
+    $sql = "SELECT what FROM person";
+    $stmt = $this->pdo->query($sql);
+    return $stmt->fetchColumn();
+  }
+}
