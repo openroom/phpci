1c176ac20deccdbcadad68fc2b4b794a01381d22
Kushal Hada <kushaldeveloper@gmail.com>
2017-08-17 06:03:48 -0400

    add user tests setup and teardown

--- Tests/UserTest.php
+++ Tests/UserTest.php
@@ -1,7 +1,37 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: panda
- * Date: 8/17/17
- * Time: 6:00 AM
- */
+
+class UserTest extends PHPUnit_Framework_TestCase
+{
+
+    /**
+     * @var PDO
+     */
+    private $pdo;
+
+    public function setUp()
+    {
+        $this->pdo = new PDO($GLOBALS['db_dsn'], $GLOBALS['db_username'], $GLOBALS['db_password']);
+        $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $createTable = "CREATE TABLE users  (
+  id               SERIAL PRIMARY KEY,
+  username         TEXT                        NOT NULL UNIQUE,
+  display_name     TEXT,
+  password         TEXT                        NOT NULL,
+  email            TEXT                        NOT NULL,
+  last_login       TIMESTAMP WITHOUT TIME ZONE NOT NULL DEFAULT (now()),
+  is_active        BOOLEAN                     NOT NULL DEFAULT FALSE,
+  is_administrator BOOLEAN                     NOT NULL DEFAULT FALSE,
+  is_reporter      BOOLEAN                     NOT NULL DEFAULT FALSE,
+  is_banned        BOOLEAN                     NOT NULL DEFAULT FALSE
+);";
+        $this->pdo->query($createTable);
+    }
+
+    public function tearDown()
+    {
+        if (!$this->pdo) {
+            return;
+        }
+        $this->pdo->query("DROP TABLE users");
+    }
+}
