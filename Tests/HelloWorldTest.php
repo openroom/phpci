fc89831534227bf35ed0e251867bd9c7f8ba4d21
Kushal Hada <kushaldeveloper@gmail.com>
2017-08-16 22:44:00 -0400

    add namespace

--- Tests/HelloWorldTest.php
+++ Tests/HelloWorldTest.php
@@ -25,21 +25,21 @@ class HelloWorldTest extends PHPUnit_Framework_TestCase
 
     public function testHelloWorld()
     {
-        $helloWorld = new HelloWorld($this->pdo);
+        $helloWorld = new \model\HelloWorld($this->pdo);
 
         $this->assertEquals('Hello World', $helloWorld->hello());
     }
 
     public function testHello()
     {
-        $helloWorld = new HelloWorld($this->pdo);
+        $helloWorld = new \model\HelloWorld($this->pdo);
 
         $this->assertEquals('Hello Bar', $helloWorld->hello('Bar'));
     }
 
     public function testWhat()
     {
-        $helloWorld = new HelloWorld($this->pdo);
+        $helloWorld = new \model\HelloWorld($this->pdo);
 
         $this->assertFalse($helloWorld->what());
 
