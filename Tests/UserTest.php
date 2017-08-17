6a1fa85e83f143cd99fc6b55caf8ddc2538a3ac6
Kushal Hada <kushaldeveloper@gmail.com>
2017-08-17 10:21:01 -0400

    assert array is empty
    Time: 7.41 seconds, Memory: 12.00MB
    
    There was 1 failure:
    
    1) UserTest::testFetchAll
    Failed asserting that Array &0 () is null.
    
    /builds/kfree/php/phpci/Tests/UserTest.php:77
    
    FAILURES!
    Tests: 18, Assertions: 11, Failures: 1.
    
    Code Coverage Report:
      2017-08-17 14:15:47
    
     Summary:
      Classes: 26.93% (296/1099)
      Methods:  1.22% (59/4830)
      Lines:    1.23% (540/44011)
    
    @PHP::PHP_CodeCoverage
      Methods:   0.00% ( 0/36)   Lines:   1.03% (  4/390)
    @PHP::PHP_Timer
      Methods:  40.00% ( 2/ 5)   Lines:  21.43% (  3/ 14)
    @PHPUnit.Framework.Constraint::PHPUnit_Framework_Constraint_IsEqual
      Methods:   0.00% ( 0/ 3)   Lines:  26.53% ( 13/ 49)
    @PHPUnit.Framework.Constraint::PHPUnit_Framework_Constraint_IsFalse
      Methods:  50.00% ( 1/ 2)   Lines:  50.00% (  1/  2)
    @PHPUnit.Framework.Constraint::PHPUnit_Framework_Constraint_IsNull
      Methods: 100.00% ( 2/ 2)   Lines: 100.00% (  2/  2)
    @PHPUnit.Framework::PHPUnit_Framework_Assert
      Methods:   4.35% ( 6/138)   Lines:   2.08% ( 17/816)
    @PHPUnit.Framework::PHPUnit_Framework_Constraint
      Methods:  57.14% ( 4/ 7)   Lines:  84.00% ( 21/ 25)
    @PHPUnit.Framework::PHPUnit_Framework_Exception
      Methods:  25.00% ( 1/ 4)   Lines:  45.45% (  5/ 11)
    @PHPUnit.Framework::PHPUnit_Framework_ExpectationFailedException
      Methods:  50.00% ( 1/ 2)   Lines:  75.00% (  3/  4)
    @PHPUnit.Framework::PHPUnit_Framework_TestCase
      Methods:   4.35% ( 4/92)   Lines:  21.23% (142/669)
    @PHPUnit.Framework::PHPUnit_Framework_TestResult
      Methods:   0.00% ( 0/54)   Lines:  11.21% ( 37/330)
    @PHPUnit.Util::PHPUnit_Util_ErrorHandler
      Methods:   0.00% ( 0/ 4)   Lines:   6.25% (  2/ 32)
    @PHPUnit.Util::PHPUnit_Util_Test
      Methods:   3.23% ( 1/31)   Lines:  19.38% ( 75/387)
    Person
      Methods: 100.00% ( 3/ 3)   Lines: 100.00% (  8/  8)
    \Composer\Autoload::ClassLoader
      Methods:   4.76% ( 1/21)   Lines:  27.69% ( 36/130)
    \SebastianBergmann\Exporter::Exporter
      Methods:  20.00% ( 1/ 5)   Lines:  15.09% ( 16/106)
    \SebastianBergmann\GlobalState::Blacklist
      Methods:  25.00% ( 2/ 8)   Lines:  10.00% (  3/ 30)
    \SebastianBergmann\GlobalState::Restorer
      Methods:  50.00% ( 2/ 4)   Lines:  47.17% ( 25/ 53)
    \SebastianBergmann\GlobalState::Snapshot
      Methods:  27.27% ( 6/22)   Lines:  38.61% ( 39/101)
    \SebastianBergmann\RecursionContext::Context
      Methods:  14.29% ( 1/ 7)   Lines:  42.86% ( 15/ 35)
    \model::HelloWorld
      Methods: 100.00% ( 3/ 3)   Lines: 100.00% (  8/  8)
    \model::User
      Methods:  52.94% (18/34)   Lines:  47.29% ( 61/129)
    ERROR: Job failed: exit code 1

--- Tests/UserTest.php
+++ Tests/UserTest.php
@@ -1,19 +1,17 @@
 <?php
 
-class UserTest extends \PHPUnit_Framework_TestCase
-{
+class UserTest extends \PHPUnit_Framework_TestCase {
 
-    /**
-     * @var PDO
-     */
-    private $pdo;
+  /**
+   * @var PDO
+   */
+  private $pdo;
 
-    public function setUp()
-    {
-        $this->pdo = new PDO($GLOBALS['db_dsn'], $GLOBALS['db_username'], $GLOBALS['db_password']);
-        $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        $this->pdo->query("DROP TABLE IF EXISTS users CASCADE");
-        $createTable = "CREATE TABLE users  (
+  public function setUp() {
+    $this->pdo = new PDO($GLOBALS['db_dsn'], $GLOBALS['db_username'], $GLOBALS['db_password']);
+    $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $this->pdo->query("DROP TABLE IF EXISTS users CASCADE");
+    $createTable = "CREATE TABLE users  (
   id               SERIAL PRIMARY KEY,
   username         TEXT                        NOT NULL UNIQUE,
   display_name     TEXT,
@@ -25,55 +23,55 @@ class UserTest extends \PHPUnit_Framework_TestCase
   is_reporter      BOOLEAN                     NOT NULL DEFAULT FALSE,
   is_banned        BOOLEAN                     NOT NULL DEFAULT FALSE
 );";
-        $this->pdo->query($createTable);
-    }
+    $this->pdo->query($createTable);
+  }
 
-    public function tearDown()
-    {
-        if (!$this->pdo) {
-            return;
-        }
-        $this->pdo->query("DROP TABLE users");
+  public function tearDown() {
+    if (!$this->pdo) {
+      return;
     }
+    $this->pdo->query("DROP TABLE users");
+  }
 
-    public function testAddAdminUser()
-    {
-        $tableName = 'users';
-        $hashedPassword = \model\User::hashPassword('hunter2');
-        $admin = \model\User::create()
-            ->setUsername("hunter")
-            ->setPassword($hashedPassword)
-            ->setDisplayName("Gunter Adams")
-            ->setEmail("hikingfan@gmail.com")
-            ->setIsActive(true)
-            ->setIsBanned(false)
-            ->setIsReporter(false)
-            ->setIsAdministrator(true);
-        \model\User::addUser($this->pdo, $admin);
-        $adminEmail = \model\User::fetchByUsername($this->pdo, "hunter")->getEmail();
-        $this->assertEquals('hikingfan@gmail.com', $adminEmail);
-    }
-    public function testAddReporterUser()
-    {
-        $tableName = 'users';
-        $hashedPassword = \model\User::hashPassword('hunter2');
-        $admin = \model\User::create()
-            ->setUsername("hunter")
-            ->setPassword($hashedPassword)
-            ->setDisplayName("Gunter Adams")
-            ->setEmail("hikingfan@gmail.com")
-            ->setIsActive(true)
-            ->setIsBanned(false)
-            ->setIsReporter(true)
-            ->setIsAdministrator(false);
-        \model\User::addUser($this->pdo, $admin);
-        $adminEmail = \model\User::fetchByUsername($this->pdo, "hunter")->getEmail();
-        $this->assertEquals('hikingfan@gmail.com', $adminEmail);
-    }
+  public function testAddAdminUser() {
+    $tableName = 'users';
+    $hashedPassword = \model\User::hashPassword('hunter2');
+    $admin = \model\User::create()
+      ->setUsername("hunter")
+      ->setPassword($hashedPassword)
+      ->setDisplayName("Gunter Adams")
+      ->setEmail("hikingfan@gmail.com")
+      ->setIsActive(TRUE)
+      ->setIsBanned(FALSE)
+      ->setIsReporter(FALSE)
+      ->setIsAdministrator(TRUE);
+    \model\User::addUser($this->pdo, $admin);
+    $adminEmail = \model\User::fetchByUsername($this->pdo, "hunter")
+      ->getEmail();
+    $this->assertEquals('hikingfan@gmail.com', $adminEmail);
+  }
+
+  public function testAddReporterUser() {
+    $tableName = 'users';
+    $hashedPassword = \model\User::hashPassword('hunter2');
+    $admin = \model\User::create()
+      ->setUsername("hunter")
+      ->setPassword($hashedPassword)
+      ->setDisplayName("Gunter Adams")
+      ->setEmail("hikingfan@gmail.com")
+      ->setIsActive(TRUE)
+      ->setIsBanned(FALSE)
+      ->setIsReporter(TRUE)
+      ->setIsAdministrator(FALSE);
+    \model\User::addUser($this->pdo, $admin);
+    $adminEmail = \model\User::fetchByUsername($this->pdo, "hunter")
+      ->getEmail();
+    $this->assertEquals('hikingfan@gmail.com', $adminEmail);
+  }
 
   public function testFetchAll() {
     $noUsers = \model\User::fetchAll($this->pdo);
     echo $noUsers;
-    $this->assertNull($noUsers);
+    $this->assertEmpty($noUsers, 'array is empty');
   }
 }
